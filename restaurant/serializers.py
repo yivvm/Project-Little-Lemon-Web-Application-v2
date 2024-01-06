bb6808f0c2695c37b38c9dc7a440adc5fc6b1d54
yivvm <134717898+yivvm@users.noreply.github.com>
2024-01-05 20:37:25 -0500

    app file

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,10 +1,8 @@
 
 from rest_framework import serializers 
 from decimal import Decimal
-# from rest_framework.validators import UniqueTogetherValidator 
 from django.contrib.auth.models import User 
 
-# from .models import Rating, Cart, Order, OrderItem 
 
 from .models import Menu, Booking, Category, MenuItem
 
@@ -40,59 +38,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'groups']
-
-
-
-# class RatingSerializer(serializers.ModelSerializer): 
-#     user = serializers.PrimaryKeyRelatedField(
-#     queryset=User.objects.all(), 
-#     default=serializers.CurrentUserDefault()
-#     )
-
-#     class Meta:
-#         model = Rating
-#         fields = ['user', 'menuitem_id', 'rating']
-
-#         validators = [
-#             UniqueTogetherValidator(
-#                 queryset = Rating.objects.all(), 
-#                 fields=['user', 'menuitem_id']
-#             )
-#         ]
-
-#         extra_kwargs = {
-#             'rating': {'max_value' : 5, 'min_value' : 0}
-#         }
-
-
-# class CartSerializer(serializers.ModelSerializer): 
-#     user = serializers.PrimaryKeyRelatedField(
-#         queryset = Rating.objects.all(), 
-#         default = serializers.CurrentUserDefault()
-#     )
-
-#     def validate(self, attributes):
-#         attributes["price"] = attributes["quantity"] * attributes["unit_price"]
-#         return attributes
-
-#     class Meta:
-#         model = Cart
-#         fields = ['user', 'menuitem', 'unit_price', 'price', 'quantity']
-#         extra_kwargs = {
-#             'price': {'read_only' : True}
-#         } 
-
-
-# class OrderItemSerializer(serializers.ModelSerializer): 
-#     class Meta:
-#         model = OrderItem
-#         fields = ['order', 'menuitem', 'quantity', 'price']
-
-
-# class OrderSerializer(serializers.ModelSerializer): 
-#     orderitem = OrderItemSerializer(many=True, read_only=True, source='order')
-
-#     class Meta:
-#         model = Order
-#         fields = ['id', 'user', 'delivery_crew', 'status', 'date', 'total', 'orderitem']
 
