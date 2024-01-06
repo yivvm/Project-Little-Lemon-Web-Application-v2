01324ccd8ec72afc945128ba383d29e2ba5b479a
yivvm <134717898+yivvm@users.noreply.github.com>
2024-01-05 20:35:58 -0500

    app file

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -3,14 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Menu, Booking
 
-# from .models import Rating, Category, MenuItem, Cart, Order, OrderItem
 
 admin.site.register(Menu)
 admin.site.register(Booking)
-
-# admin.site.register(Rating)
-# admin.site.register(Category)
-# admin.site.register(MenuItem)
-# admin.site.register(Cart)
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
