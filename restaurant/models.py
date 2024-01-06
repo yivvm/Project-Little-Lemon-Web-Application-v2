d8d63d78b7b840af180044a9d945941e3e6e9bae
yivvm <134717898+yivvm@users.noreply.github.com>
2024-01-05 20:33:10 -0500

    Add files via upload

--- restaurant/models.py
+++ restaurant/models.py
@@ -37,33 +37,3 @@ class MenuItem(models.Model):
     def __str__(self) -> str:
         return self.title
 
-
-# class Cart(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-#     quantity = models.SmallIntegerField()
-#     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-
-#     class Meta: 
-#         unique_together = ('menuitem', 'user')
-
-
-# class Order(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
-#     status = models.BooleanField(db_index=True, default=0) 
-#     total = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-#     date = models.DateField(db_index=True)
-
-
-# class OrderItem(models.Model):
-#     order = models.ForeignKey(User, on_delete=models.CASCADE)
-#     # order = models.ForeignKey(User, on_delete=models.CASCADE, related_name="order") 
-#     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-#     quantity = models.SmallIntegerField()
-#     unnit_price = models.DecimalField(max_digits=6, decimal_places=2)
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-
-#     class Meta: 
-#         unique_together = ('order', 'menuitem')
