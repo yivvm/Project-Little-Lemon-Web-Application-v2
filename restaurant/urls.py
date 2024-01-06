94c1a4147c14356f630a83fb6af626689c6ea51b
yivvm <134717898+yivvm@users.noreply.github.com>
2024-01-05 20:40:08 -0500

    app file

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -11,27 +11,10 @@ urlpatterns = [
     path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
     path('bookings', views.bookings, name='bookings'), 
 
-    # path('menu', views.MenuView.as_view()),
     path('booking', views.BookingView.as_view()),
 
     path('menu-items', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('categories', views.CategoriesView.as_view()),
 
-    # path('ratings', views.RatingsView.as_view()),
-    # path('cart/menu-items', views.CartView.as_view()),
-    # path('orders', views.OrdersView.as_view()),
-    # path('orders/<int:pk>', views.SingleOrderView.as_view()),
-    # path(
-    #     'groups/manager/users', 
-    #     views.GroupViewSet.as_view(
-    #         {'get': 'list', 'post': 'create', 'delete': 'destroy'}
-    #     )
-    # ),
-    # path(
-    #     'groups/delivery-crew/users',
-    #     views.DeliveryCrewViewSet.as_view(
-    #         {'get': 'list', 'post': 'create', 'delete': 'destroy'}
-    #     )
-    # ),
 ]
